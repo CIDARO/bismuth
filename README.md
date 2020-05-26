1fd27e7187cadd1faa030487ce53af6411b3e177
PaoloRollo <paolo.rollo@laserromae.it>
2020-05-26 12:42:38 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,36 @@
+<div align="center">
+  <br/>
+  <img src="./bismuth.png" width="200" />
+  <br/>
+  <br/>
+  <p>
+    Atom that visualizes the current CIDARO srl microservices infrastructure.
+  </p>
+  <p>
+    version 1.0.0-beta
+  </p>
+  <br/>
+  <p>
+    <a href="#status"><strong>Status</strong></a> ·
+    <a href="#description"><strong>Description</strong></a> ·
+    <a href="#install"><strong>Install</strong></a>
+  </p>
+</div>
+
+---
+
+## Status
+
+**Bismuth** is currently in **beta** version and it is currently visible at <a href="https://bismuth.cidaro.com">this link</a>.
+
+---
+
+## Description
+
+**Bismuth** is a simple React App that visualizes using the d3.js library a graph that displays our microservices infrastructure.
+
+---
+
+## Install
+
+TBC
---
+++ src/App.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import { Graph } from "react-d3-graph";
+import dockerSvg from './docker.svg';
+import mongodbSvg from './mongodb.svg';
+import nodejsSvg from './nodejs.svg'
+import redisSvg from './redis.svg';
+
+const data = {
+  nodes: [
+    {id: 'Oxygen API', svg: nodejsSvg}, 
+    {id: 'Oxygen Cache', svg: redisSvg}, 
+    {id: 'Carbon', svg: dockerSvg}, 
+    {id: 'Carbon DB', svg: mongodbSvg}, 
+    {id: 'Carbon Cache', svg: redisSvg}, 
+    {id: 'Carbon Fluorine', svg: dockerSvg},
+    {id: 'Copper', svg: dockerSvg},
+    {id: 'Copper DB', svg: mongodbSvg},
+    {id: 'Helium', svg: dockerSvg},
+    {id: 'Helium DB', svg: mongodbSvg},
+    {id: 'Helium Cache', svg: redisSvg},
+    {id: 'Helium Fluorine', svg: dockerSvg},
+    {id: 'Gold', svg: dockerSvg},
+    {id: 'Redis Queues', svg: redisSvg}
+  ],
+  links: [
+    {source: 'Oxygen API', target: 'Oxygen Cache'},
+    {source: 'Oxygen API', target: 'Carbon'},
+    {source: 'Oxygen API', target: 'Helium'},
+    {source: 'Carbon', target: 'Carbon DB'},
+    {source: 'Carbon', target: 'Carbon Cache'},
+    {source: 'Carbon', target: 'Carbon Fluorine'},
+    {source: 'Carbon', target: 'Redis Queues'},
+    {source: 'Copper', target: 'Copper DB'},
+    {source: 'Copper', target: 'Redis Queues'},
+    {source: 'Helium', target: 'Helium DB'},
+    {source: 'Helium', target: 'Helium Cache'},
+    {source: 'Helium', target: 'Helium Fluorine'},
+    {source: 'Helium', target: 'Redis Queues'},
+    {source: 'Gold', target: 'Redis Queues'},
+  ]
+};
+
+const config = {
+  nodeHighlightBehavior: true,
+  linkHighlightBehavior: true,
+  directed: true,
+  height: window.innerHeight,
+  width: window.innerWidth,
+  minZoom: 1,
+  automaticRearrangeAfterDropNode: true,
+  node: {
+    fontWeight: 'bold'
+  }
+}
+
+export default class App extends Component {
+
+  render() {
+    return (
+      <div className="App flex flex-col items-center justify-center h-full">
+        <Graph 
+          id="graph" 
+          data={data} 
+          config={config}
+        />
+      </div>
+    );
+  }
+}
